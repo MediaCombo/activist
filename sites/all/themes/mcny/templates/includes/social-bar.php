9e97325127a58f8978e35f99e83a23050fb6650d
Mirza Arslan Baig <arslan.baig@tkxel.com>
2016-05-04 17:01:53 +0500

    social region removed and social button make work as greatest grid have.

--- sites/all/themes/mcny/templates/includes/social-bar.php
+++ sites/all/themes/mcny/templates/includes/social-bar.php
@@ -1,9 +1,21 @@
-<?php if($page['social_bar']): ?>
-    <div class="row">
-        <div class="container">
-            <div class="col-lg-12 margin-bottom-10">
-                <?php echo render($page['social_bar']); ?>
-            </div>
+<div class="row">
+    <div class="container">
+        <div class="col-lg-12 margin-bottom-10">
+            <!-- Go to www.addthis.com/dashboard to customize your tools -->
+            <p><div class="addthis_native_toolbox"></div></p>
+<!--            <p><a class="btn btn-default btn-share" href="#" title="Share"><span class="fa fa-plus"></span>Share</a></p>-->
         </div>
     </div>
-<?php endif; ?>
+</div>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-555991b64394b0fe" async="async"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-67803566-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
